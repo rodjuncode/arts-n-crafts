717900ffeb0797dad0a4e84aec9f297150a6f47e
Rodrigo Junqueira <rodrigo.fpjunqueira@gmail.com>
2020-12-15 18:43:05 -0300

    Beta version stable.

--- Tile.js
+++ Tile.js
@@ -4,7 +4,6 @@ function Tile(puzzle,x,y) {
     this.y = y;
 
     this.show = function() {
-        //tint("#4569b1");
         image(this.puzzle.art, 0, 0, 
             this.puzzle.tileWidth, this.puzzle.tileHeight, 
             this.puzzle.tileWidth*this.x, this.puzzle.tileHeight*this.y, 
